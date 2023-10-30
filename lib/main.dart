738717bb8b36ef742607ce5141f1ed1bc68f9772
Astrum93 <jungmin2670@gmail.com>
2023-10-30 17:15:25 +0900

    231030

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          // actions: [
-          //   IconButton(
-          //     onPressed: () {
-          //       print('Tab!');
-          //     },
-          //     icon: const Icon(Icons.home),
-          //   ),
-          //   const Icon(Icons.play_arrow),
-          // ],
-          centerTitle: true,
-          title: const Text('This is App bar'),
-        ),
-        body: const CustomContainer(),
-        // floatingActionButton: FloatingActionButton(
-        //   child: const Icon(Icons.bug_report),
-        //   onPressed: () {
-        //     print('Tab FAB!');
-        //   },
-        // ),
-      ),
+    const MaterialApp(
+      home: CustomContainer(),
     ),
   );
+}
+
+class FirstScaffold extends StatelessWidget {
+  const FirstScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('Tab!');
+            },
+            icon: const Icon(Icons.home),
+          ),
+          const Icon(Icons.play_arrow),
+        ],
+        centerTitle: true,
+        title: const Text('This is App bar'),
+      ),
+      body: const Text('Hello World'),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.bug_report),
+        onPressed: () {
+          print('Tab FAB!');
+        },
+      ),
+    );
+  }
 }
 
 class TestWidget extends StatelessWidget {
@@ -34,14 +43,16 @@ class TestWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      // 위쪽 공간 확보하는 위젯
-      child: Center(
-        child: Text(
-          'Hello, Flutter',
-          style: TextStyle(
-            fontSize: 60,
-            color: Colors.black,
+    return const Scaffold(
+      body: SafeArea(
+        // 위쪽 공간 확보하는 위젯
+        child: Center(
+          child: Text(
+            'Hello, Flutter',
+            style: TextStyle(
+              fontSize: 60,
+              color: Colors.black,
+            ),
           ),
         ),
       ),
@@ -54,33 +65,39 @@ class CustomContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 300,
-        height: 300,
-        padding: const EdgeInsets.fromLTRB(10, 12, 10, 12),
-        decoration: BoxDecoration(
-          color: const Color(0xFF85D07B),
-          border:
-              Border.all(color: Colors.red, width: 5, style: BorderStyle.solid),
-          borderRadius: BorderRadius.circular(100),
-          boxShadow: const [
-            BoxShadow(
-                color: Colors.grey,
-                offset: Offset(6, 6),
-                blurRadius: 10,
-                spreadRadius: 10),
-            BoxShadow(
-                color: Colors.blue,
-                offset: Offset(-6, -6),
-                blurRadius: 10,
-                spreadRadius: 10),
-          ],
-        ),
-        child: Center(
-          child: Container(
-            color: Colors.yellow,
-            child: const Text('Hello Container'),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('This is App bar'),
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 300,
+          padding: const EdgeInsets.fromLTRB(10, 12, 10, 12),
+          decoration: BoxDecoration(
+            color: const Color(0xFF85D07B),
+            border: Border.all(
+                color: Colors.red, width: 5, style: BorderStyle.solid),
+            borderRadius: BorderRadius.circular(100),
+            boxShadow: const [
+              BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(6, 6),
+                  blurRadius: 10,
+                  spreadRadius: 10),
+              BoxShadow(
+                  color: Colors.blue,
+                  offset: Offset(-6, -6),
+                  blurRadius: 10,
+                  spreadRadius: 10),
+            ],
+          ),
+          child: Center(
+            child: Container(
+              color: Colors.yellow,
+              child: const Text('Hello Container'),
+            ),
           ),
         ),
       ),
